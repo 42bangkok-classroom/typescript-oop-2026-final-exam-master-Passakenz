f7200c7318b1f154dbfbb36845f1ef4bee7b17a4
event <event@z2t16c3.42bangkok.com>
2026-03-25 12:04:38 +0700

    p01-2

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -5,10 +5,10 @@ import * as path from 'path';
 
 @Injectable()
 export class ProductService {
-  findAll(): Product[] {
+  findAll(): Promise<Product> {
     const filePath = path.join(process.cwd(), 'data', 'products.json');
     const raw = fs.readFileSync(filePath, 'utf-8');
-    const ProD: Product[] = JSON.parse(raw);
+    const ProDuctS: Product[] = JSON.parse(raw);
 
     return this.findAll();
   }
